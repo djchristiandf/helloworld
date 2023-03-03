ac5f05b9cf03dba514126c61b0ecf353564b9d82
djchristiandf <djchristiandf@gmail.com>
2023-03-03 16:41:42 -0300

    adicionando o type script automtizar

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import express, {Request, Response, NextFunction} from 'express';
+//const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({foo: 'sucesso total, com ts-dev!'});
+})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
