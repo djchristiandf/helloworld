9140a71cdc674bf12df01845efac77b130afcf39
djchristiandf <djchristiandf@gmail.com>
2023-03-03 21:18:27 -0300

    criando crud de usuarios sem popular no banco

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,8 +1,12 @@
 import express, {Request, Response, NextFunction} from 'express';
+import usersRoute from '../routes/users.route';
 //const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+app.use(usersRoute)
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({foo: 'sucesso total, com ts-dev!'});
 })
