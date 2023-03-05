0e6152d3427ee0ab284b096386cf1c1d5b39dd31
djchristiandf <djchristiandf@gmail.com>
2023-03-04 21:13:05 -0300

    finalizando a parte de status

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response, NextFunction} from 'express';
 import usersRoute from '../routes/users.route';
+import statusRoute from '../routes/status.route';
+
 //const express = require('express')
 const app = express()
 const port = 3000
@@ -7,7 +9,6 @@ const port = 3000
 app.use(express.json());
 
 app.use(usersRoute)
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({foo: 'sucesso total, com ts-dev!'});
-})
+app.use(statusRoute)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
